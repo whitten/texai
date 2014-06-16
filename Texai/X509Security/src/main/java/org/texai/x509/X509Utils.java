2540b26909efc2675d29da73edbcb2d8c6f26480
StephenLReed <stephenreed@yahoo.com>
2014-06-16 18:20:40 -0500

    Initial commit of the X509Security module.

--- Texai/X509Security/src/main/java/org/texai/x509/X509Utils.java
+++ Texai/X509Security/src/main/java/org/texai/x509/X509Utils.java
@@ -18,6 +18,11 @@
  * You should have received a copy of the GNU General Public License along with this program;
  * if not, write to the Free Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  */
+
+
+// refer to http://www.bouncycastle.org/docs/docs1.5on/index.html for substitutes for deprecated references
+
+
 package org.texai.x509;
 
 import java.io.BufferedReader;
