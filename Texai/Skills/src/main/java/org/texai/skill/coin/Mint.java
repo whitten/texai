1c12d9609b3af0197d72c099113cbcaf6a26a975
StephenLReed <stephenreed@yahoo.com>
2014-08-28 22:03:51 -0500

    Added shell command execution  of bitcoin-cli .

--- Texai/Skills/src/main/java/org/texai/skill/coin/Mint.java
+++ Texai/Skills/src/main/java/org/texai/skill/coin/Mint.java
@@ -1,9 +1,14 @@
 package org.texai.skill.coin;
 
+import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
 import org.apache.log4j.Logger;
 import org.texai.ahcsSupport.AHCSConstants;
 import org.texai.ahcsSupport.AbstractSkill;
 import org.texai.ahcsSupport.Message;
+import org.texai.util.EnvironmentUtils;
+import org.texai.util.TexaiException;
 
 /**
  * Created on Aug 28, 2014, 8:36:17 PM.
@@ -16,29 +21,40 @@ import org.texai.ahcsSupport.Message;
  *
  * Copyright (C) 2014 Texai
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
-
 public class Mint extends AbstractSkill {
 
-  /** the logger */
+  /**
+   * the logger
+   */
   private static final Logger LOGGER = Logger.getLogger(Mint.class);
+  /**
+   * the timer
+   */
+  private final Timer mintTimer;
 
-  /** Receives and attempts to process the given message.  The skill is thread safe, given that any contained libraries are single threaded
+  /**
+   * Constructs a new Mint instance.
+   */
+  public Mint() {
+    mintTimer = new Timer(
+            "mint timer", // name
+            true); // isDaemon
+  }
+
+  /**
+   * Receives and attempts to process the given message. The skill is thread safe, given that any contained libraries are single threaded
    * with regard to the conversation.
    *
    * @param message the given message
+   *
    * @return whether the message was successfully processed
    */
   @Override
@@ -69,21 +85,26 @@ public class Mint extends AbstractSkill {
                 operation);
         setSkillState(AHCSConstants.State.READY);
         return true;
+
+      case AHCSConstants.MINT_NEW_BLOCKS_TASK:
+        assert this.getSkillState().equals(AHCSConstants.State.READY) : "prior state must be ready";
+        mintNewBlocks();
+        return true;
     }
 
     assert getSkillState().equals(AHCSConstants.State.READY) : "must be in the ready state";
 
     // other operations ...
-
-
     sendMessage(notUnderstoodMessage(message));
     return true;
   }
 
-  /** Synchronously processes the given message.  The skill is thread safe, given that any contained libraries are single threaded
-   * with regard to the conversation.
+  /**
+   * Synchronously processes the given message. The skill is thread safe, given that any contained libraries are single threaded with regard
+   * to the conversation.
    *
    * @param message the given message
+   *
    * @return the response message or null if not applicable
    */
   @Override
@@ -92,21 +113,74 @@ public class Mint extends AbstractSkill {
     assert message != null : "message must not be null";
 
     //TODO handle operations
-
     return notUnderstoodMessage(message);
   }
 
-  /** Returns the understood operations.
+  /**
+   * Returns the understood operations.
    *
    * @return the understood operations
    */
   @Override
   public String[] getUnderstoodOperations() {
     return new String[]{
-              AHCSConstants.MESSAGE_NOT_UNDERSTOOD_INFO,
-              AHCSConstants.AHCS_INITIALIZE_TASK,
-              AHCSConstants.AHCS_READY_TASK
-            };
+      AHCSConstants.MESSAGE_NOT_UNDERSTOOD_INFO,
+      AHCSConstants.AHCS_INITIALIZE_TASK,
+      AHCSConstants.AHCS_READY_TASK,
+      AHCSConstants.MINT_NEW_BLOCKS_TASK
+    };
   }
 
+  /**
+   * Mints a new Bitcoin block every 10 minutes.
+   */
+  private void mintNewBlocks() {
+    LOGGER.info("mint new blocks");
+    mintTimer.scheduleAtFixedRate(
+            null,
+            0l, // delay,
+            10000l); // period - 10 minutes
+  }
+
+  /**
+   * Provides a mint timer task
+   */
+  class MintTimerTask extends TimerTask {
+
+    /**
+     * Executes this timer task
+     */
+    @Override
+    public void run() {
+      generateNewBlock();
+    }
+  }
+
+  /**
+   * Uses the Bitcoin command line interface to generate a new block.
+   */
+  private void generateNewBlock() {
+    //Preconditions
+    if (!EnvironmentUtils.isLinux()) {
+      throw new TexaiException("Operating system must be Linux");
+    }
+
+    String[] cmdArray = {
+      "sh",
+      "-c",
+      ""
+    };
+    //toGraphViz(System.getProperty("user.dir") + "/doc/graph-visualization", graphName, focalQueryContainerName);
+    final StringBuilder stringBuilder = new StringBuilder();
+    stringBuilder.append("cd ");
+    stringBuilder.append(System.getProperty("user.dir"));
+    stringBuilder.append("git/bitcoin/src/ ; ./bitcoin-cli setgenerate true");
+    cmdArray[2] = stringBuilder.toString();
+    LOGGER.info("shell cmd: " + cmdArray[2]);
+    try {
+      Runtime.getRuntime().exec(cmdArray);
+    } catch (final IOException ex) {
+      throw new TexaiException(ex);
+    }
+  }
 }
