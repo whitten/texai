6c5f7b5e0873d6fe3a929533e0daee8116697894
Stephen Reed <stephenreed@yahoo.com>
2015-11-30 13:11:19 -0600

    Added code to the PhotoApp project.

---
+++ Texai/PhotoApp/src/test/java/org/texai/photoapp/PhotoAppServerTest.java
@@ -0,0 +1,18 @@
+package org.texai.photoapp;
+
+/**
+ * PhotoAppServerTest.java
+ *
+ * Description:
+ *
+ * Copyright (C) Nov 30, 2015, Stephen L. Reed.
+ */
+public class PhotoAppServerTest {
+
+  /**
+   * Creates a new instance of PhotoAppServerTest.
+   */
+  public PhotoAppServerTest() {
+  }
+
+}
---
+++ Texai/PhotoApp/src/test/java/org/texai/photoapp/TestPhotoAppActions.java
@@ -0,0 +1,18 @@
+package org.texai.photoapp;
+
+/**
+ * TestPhotoAppActions.java
+ *
+ * Description:
+ *
+ * Copyright (C) Nov 30, 2015, Stephen L. Reed.
+ */
+public class TestPhotoAppActions {
+
+  /**
+   * Creates a new instance of TestPhotoAppActions.
+   */
+  public TestPhotoAppActions() {
+  }
+
+}
